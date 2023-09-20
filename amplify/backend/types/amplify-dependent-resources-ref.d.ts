ff825455cd1b204e10c79acb2a22c0d8c37990ac
josefaidt <josef.aidt@gmail.com>
2023-09-20 12:35:28 -0700

    add headers, sample fetch

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyappsynccors": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifyappsynccors2e2fa129": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
+import { Authenticator } from '@aws-amplify/ui-react'
+import { Amplify } from 'aws-amplify'
+import config from '@/aws-exports'
+import '@aws-amplify/ui-react/styles.css'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+Amplify.configure(config)
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    // <Authenticator>
+    //   {({ signOut, user }) => (
+    //     <main>
+    //       <h1>Hello {user!.username}</h1>
+    //       <button onClick={signOut}>Sign out</button>
+    <Component {...pageProps} />
+    //     </main>
+    //   )}
+    // </Authenticator>
+  )
 }
