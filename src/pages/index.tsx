4bef35a73e6c1d2aac42428402a374e1a2e291c6
josefaidt <josef.aidt@gmail.com>
2023-09-21 13:03:38 -0700

    rm unused "reload" reference

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,10 @@ export default function Home() {
             {JSON.stringify(todos || {}, null, 2)}
           </code>
         </p>
-        <button className="border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <button
+          className="border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+          onClick={() => {}}
+        >
           Reload
         </button>
       </div>
